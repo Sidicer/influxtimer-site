20b4944d7c3465433a3cb4e232de638b28d5741f
TotallyMehis <totally.mehis@gmail.com>
2019-07-12 22:04:21 +0300

    js dumbness begone

--- www/js/randbg.js
+++ www/js/randbg.js
@@ -1,7 +1,6 @@
 const imgs = ['bg001.jpg', 'bg002.jpg', 'bg003.jpg', 'bg004.jpg'];
 
 $(document).ready(function() {
-	console.log('hello');
 	const img = imgs[Math.floor(Math.random() * imgs.length)];
 	$('#header-imgs').css('background-image', 'url(img/'+img+')');
 } );
